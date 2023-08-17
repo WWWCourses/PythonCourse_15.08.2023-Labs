5c2b28d87e4cf7c3abb40184432b47143d62fa74
WWWCourses <ivawebcourses@gmail.com>
2023-08-17 21:03:00 +0300

    Lab2: 17.08.2023 DONE: Lecture 2

---
+++ lab2/main.py
@@ -0,0 +1,90 @@
+# this is demo for variables
+# user_name = "Jhon" #(snake-case : convention for python)
+# User_name = "Jhon" #(snake-case)
+# userName = "Jhon" #(lower Camel Case)
+# UserName = "Jhon" #(Camel Case)
+
+# -------------------------------- Data Types -------------------------------- #
+# Numerical
+# print(3)
+# print(3.14)
+# print(-3.14)
+# print(+3.14)
+
+# # Strings - низове
+# print('x♡ jdskfsk jfdskds')
+
+# print('')
+# print(' ')
+
+# print( 3+3 )
+# print( '3'+'3' )
+# print( '3'+"3" )
+# print( "abc" )
+
+# --------------------------- Aritmetic Operations --------------------------- #
+# print( 4/3 )
+
+# print( 4//3 ) # 1
+# print( 4%3 )  # 1
+
+# print(5//3)   # 1
+# print(5%3)	  # 2
+
+# ---------------------------- Assignment Operator --------------------------- #
+# a = 3
+# b = a
+# a = 5
+
+
+# print(b)  # 3 | 5 ?
+
+
+# --------------------------- Comparison Operations -------------------------- #
+# user_age = 19
+
+# print( user_age+18) # 37
+# print( user_age>18) # True
+# print( user_age==18) # False
+# print( user_age!=18) # True
+# print( user_age>=18 ) # True
+
+user_age = 19
+user_language = "en"
+
+
+# # # if user is adult AND user speaks english:
+# if user_language=="en" and user_age>=18:
+# 	print("Welcome")
+
+
+# if user_age>=18 and user_language=="en" :
+# 	print("Welcome")
+
+
+# print( True and False) # False
+# print( False  and True) # False
+
+# print(not False)
+
+# if user_language=="en" and not user_age>=18:
+# 	print("Welcome")
+
+
+# print( not user_age>=18 )
+
+# ----------------------- Shorthand Assignment Operator ---------------------- #
+
+# user_age = 19
+
+# # increment user_age with 1
+# # user_age = user_age+1
+# user_age+=1
+
+# print(user_age) #
+
+x = 5
+
+x%=3 # x = x%3
+
+print(x) # 2
