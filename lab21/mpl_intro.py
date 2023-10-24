9eb4d54ea0a7282beb910c3d12ce6025731c740d
WWWCourses <ivawebcourses@gmail.com>
2023-10-24 21:08:29 +0300

    Lab21: 24.10.2023 DONE:  Matplotlib_Intro

--- lab21/mpl_intro.py
+++ lab21/mpl_intro.py
@@ -1,18 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# ---------------------------------- Example --------------------------------- #
 #On X axis => numbers from 1 to 15
-x = np.arange(1,16)
+# x = np.arange(1,16)
 
-#On Y axis => the power of x-es
-y = x**2
+# #On Y axis => the power of x-es
+# y = x**2
 
-print(f'x: {x}')
-print(f'y: {y}')
+# print(f'x: {x}')
+# print(f'y: {y}')
 
-# let's plot the points:
-plt.plot(x,y)
+# # let's plot the points:
+# plt.plot(x,y)
 
-# if we want to see the plot and we do not use the '%matplotlib inline' magic, we have to use plt.show(). It is a good practice to use it.
+# # if we want to see the plot and we do not use the '%matplotlib inline' magic, we have to use plt.show(). It is a good practice to use it.
+# plt.show()
+
+# ------------------------------- Create Figure ------------------------------ #
+fig = plt.figure()
 plt.show()
-
---
+++ tmp.py
@@ -0,0 +1,5 @@
+def subplots():
+	return (2,3)
+
+x, y = subplots()
+print(x, y)
