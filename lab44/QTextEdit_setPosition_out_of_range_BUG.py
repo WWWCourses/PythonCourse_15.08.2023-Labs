f5368dd5ba2c668bffb03e442aacbaf276c383f7
WWWCourses <ivawebcourses@gmail.com>
2024-01-16 22:10:00 +0200

    attempt to fix "QTextCursor::setPosition out of range"

---
+++ lab44/QTextEdit_setPosition_out_of_range_BUG.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt6 import QtWidgets as qtw
+from PyQt6 import QtCore as qtc
+from PyQt6 import QtGui as qtg
+
+class MainWindow(qtw.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        editor = qtw.QTextEdit( )
+        # editor.setPlainText("a")
+        self.setCentralWidget(editor)
+
+
+        ### Fix Bug: Attempt 1: setCursorPosition to 0:
+        # editor.textCursor().setPosition(0)
+        # print(f'cursor position: {editor.textCursor().position()}')
+
+
+        ### Fix Bug: Attempt 2: move coursor to start of line
+        # cursor = editor.textCursor()
+        # cursor.movePosition(qtg.QTextCursor.MoveOperation.StartOfLine)
+        # editor.setTextCursor(cursor)
+        # print(f'cursor position: {editor.textCursor().position()}')
+
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication(sys.argv);
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
